aae3235b0a12b8729aadbb3864af7e2e99fc97cc
vkso <yuchenv@qq.com>
2023-03-31 23:02:57 +0800

    20230331 An exercise a day finished. No. 373 and 1255 not finished.

--- src/Solution.java
+++ src/Solution.java
@@ -4621,6 +4621,32 @@ public class Solution {
     }
 
     /**
+     * No. 2367 算数三元组的数目
+     * @param nums
+     * @param diff
+     * @return
+     */
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; ++i) {
+            for (int j = i + 1; j < nums.length - 1; ++j) {
+                if (nums[j] - nums[i] != diff) {
+                    continue;
+                }
+                for (int k = j + 1; k < nums.length; ++k) {
+                    if (nums[k] - nums[j] == diff) {
+                        ++count;
+                    }
+                    if (nums[k] - nums[j] > diff) {
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
      * No. 2373 矩阵中的局部最大值
      * @param grid
      * @return

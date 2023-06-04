f326533f93651820e43d055c6018611505ccec79
vkso <yuchenv@qq.com>
2023-06-05 00:27:27 +0800

    20230605

--- src/Solution.java
+++ src/Solution.java
@@ -6795,6 +6795,33 @@ public class Solution {
     }
 
     /**
+     * No. 2460 对数组执行操作
+     * @param nums
+     * @return
+     */
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                nums[left++] = nums[right];
+            }
+        }
+
+        while(left < nums.length) {
+            nums[left++] = 0;
+        }
+
+        return nums;
+    }
+
+    /**
      * No. 2465 不同的平均值数目
      *
      * @param nums

3d454bbdedc9d95836c0bd53ef2fc5998f42ded3
vkso <yuchenv@qq.com>
2023-06-16 12:49:35 +0800

    20230616

--- src/Solution.java
+++ src/Solution.java
@@ -3918,6 +3918,27 @@ public class Solution {
     }
 
     /**
+     * No. 1035 不相交的线
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    /**
      * No. 1041 困于环中的机器人
      *
      * @param instructions

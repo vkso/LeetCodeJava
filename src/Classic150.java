349ffb4f84d0637c645470e07182e48802255e33
vkso <yuchenv@qq.com>
2025-04-16 00:42:38 +0800

    20250416 Classic 150

--- src/Classic150.java
+++ src/Classic150.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 public class Classic150 {
 
     /**
+     * 数组 / 字符串
+     */
+
+    /**
      * 88. 合并两个有序数组
      * @param nums1
      * @param m
@@ -190,10 +194,90 @@ public class Classic150 {
      * @param prices
      * @return
      */
-    public int maxProfitX(int[] prices) {
+    public static int maxProfitX(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int ans = 0;
+
+        // 【贪心算法】可以假设，只要前一天的股票买入，今天卖出有利润，那么就最小化的将每一天的利润都累计到最终的结果中
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                ans += (prices[i] - prices[i-1]);
+            }
+        }
+
+        return ans;
+    }
+
+
+    /**
+     * 双指针
+     */
+
+    /**
+     * 125. 验证回文串
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome(String s) {
+        // 将字符串大写字符转换为小写字符
+        String lowerCase = s.toLowerCase();
+
+        // 移除所有的非字母数字字符
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char ch = lowerCase.charAt(i);
+            if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9') {
+                sb.append(ch);
+            }
+        }
+
+        String newStr = sb.toString();
+        if (newStr == null) {
+            return true;
+        }
+
+        for (int i = 0, j = newStr.length() - 1; i < j; i++, j--) {
+            if (newStr.charAt(i) == newStr.charAt(j)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindromeX(String s) {
+        StringBuffer sb = new StringBuffer();
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+
+        StringBuffer reverseSB = new StringBuffer(sb).reverse();
+        return sb.toString().contentEquals(reverseSB);
+    }
+
+    /**
+     * 392. 判断子序列
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
 
     }
 
+
     public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        boolean palindrome = isPalindrome(str);
     }
 }

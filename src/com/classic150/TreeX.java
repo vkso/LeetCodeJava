5864c3614adddd2f1c66d463e858310db936d997
vkso <yuchenv@qq.com>
2025-04-23 22:30:40 +0800

    20250423 TreeX

--- src/com/classic150/TreeX.java
+++ src/com/classic150/TreeX.java
@@ -18,4 +18,40 @@ public class TreeX {
             return Math.max(leftHeight, rightHeight) + 1;
         }
     }
+
+    /**
+     * No. 100 相同的树
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+    /**
+     * No. 226 翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+
+        root.left = rightNode;
+        root.right = leftNode;
+
+        return root;
+    }
 }

e67a45ddaa414d8a67f33a855db5957e349ce17c
vkso <yuchenv@qq.com>
2025-04-24 09:25:13 +0800

    20250424 MathX

--- src/com/classic150/MathX.java
+++ src/com/classic150/MathX.java
@@ -1,5 +1,7 @@
 package com.classic150;
 
+import java.util.ArrayList;
+
 public class MathX {
     /**
      * No. 9 回文数
@@ -24,5 +26,32 @@ public class MathX {
         }
 
         return true;
+    }
+
+    /**
+     * No. 66 加一
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        int push = 1;
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = digits.length - 1; i >= 0; i--) {
+            push += digits[i];
+            res.add(push % 10);
+            push /= 10;
+        }
+
+        if (push == 1) {
+            res.add(1);
+        }
+
+        int[] ints = new int[res.size()];
+
+        for (int i = 0; i < res.size(); i++) {
+            ints[i] = res.get(res.size() - 1 - i);
+        }
+
+        return ints;
     }
 }

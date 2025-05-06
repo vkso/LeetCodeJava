a7a89d472cfcd476827078da8519ae8119a836b2
vkso <yuchenv@qq.com>
2025-05-06 21:23:06 +0800

    20250505

--- src/Solution.java
+++ src/Solution.java
@@ -3489,6 +3489,26 @@ public class Solution {
     }
 
     /**
+     * No. 799 多米诺和拖米诺平铺
+     * // 动态规划，递推
+     * @param n
+     * @return
+     */
+    public int numTilings(int n) {
+        int MOD = 1000000007;
+
+        int[][] dp = new int[n + 1][4];
+        dp[0][3] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][3];
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][3] = (((dp[i - 1][0] + dp[i - 1][1]) % MOD + dp[i - 1][2]) % MOD + dp[i - 1][3]) % MOD;
+        }
+        return dp[n][3];
+    }
+
+    /**
      * No. 821 字符的最短距离
      * 假定最近字符的索引是 idx
      * 1. 计算当前字符距离 左侧 最近目标字符的距离:  currentIndex - idx

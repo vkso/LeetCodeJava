24f725a6f0f24c96427ccc6ae82e81ff7dda9384
vkso <yuchenv@qq.com>
2025-05-28 11:00:04 +0800

    20250528

--- src/Solution.java
+++ src/Solution.java
@@ -9115,6 +9115,56 @@ public class Solution {
     }
 
     /**
+     * No. 3372 连接两棵树后最大目标节点数目 I
+     * @param edges1
+     * @param edges2
+     * @param k
+     * @return
+     */
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        int max2 = 0;
+        if (k > 0) {
+            List<Integer>[] g = build_tree_3372(edges2);
+            for (int i = 0; i < edges2.length + 1; i++) {
+                max2 = Math.max(max2, dfs_3372(i, -1, 0, g, k - 1));
+            }
+        }
+
+        List<Integer>[] g = build_tree_3372(edges1);
+        int[] ans = new int[edges1.length + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = dfs_3372(i, -1, 0, g, k) + max2;
+        }
+        return ans;
+    }
+
+    public List<Integer>[] build_tree_3372(int[][] edges) {
+        List<Integer>[] g = new ArrayList[edges.length + 1];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (int[] e : edges) {
+            int x = e[0];
+            int y = e[1];
+            g[x].add(y);
+            g[y].add(x);
+        }
+        return g;
+    }
+
+    public int dfs_3372(int x, int fa, int d, List<Integer>[] g, int k) {
+        if (d > k) {
+            return 0;
+        }
+        int cnt = 1;
+        for (int y : g[x]) {
+            if (y != fa) {
+                cnt += dfs_3372(y, x, d + 1, g, k);
+            }
+        }
+        return cnt;
+    }
+
+
+    /**
      * No. 3392 统计符合条件长度为 3 的子数组数目
      * @param nums
      * @return

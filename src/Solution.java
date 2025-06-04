923a4f7aa272d39ace3009cbbeceaa777a51628c
vkso <yuchenv@qq.com>
2025-06-04 10:32:48 +0800

    20250604

--- src/Solution.java
+++ src/Solution.java
@@ -9273,6 +9273,28 @@ public class Solution {
         }
         return count;
     }
+
+    /**
+     * No. 3403 从盒子中找出字典序最大的字符串 I
+     * @param word
+     * @param numFriends
+     * @return
+     */
+    public String answerString(String word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+
+        int n = word.length();
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            String s = word.substring(i, Math.min(i + n - numFriends + 1, n));
+            if (res.compareTo(s) <= 0) {
+                res = s;
+            }
+        }
+        return res;
+    }
 }
 
 

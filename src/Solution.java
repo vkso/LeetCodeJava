8fb12e5581e1577e691e9a921158539512d8f93f
vkso <yuchenv@qq.com>
2025-06-18 23:01:55 +0800

    20250618

--- src/Solution.java
+++ src/Solution.java
@@ -9120,6 +9120,26 @@ public class Solution {
     }
 
     /**
+     * No. 2966 划分数组并满足最大差限制
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int[][] ans = new int[n / 3][];
+        for (int i = 2; i < n; i += 3) {
+            if (nums[i] - nums[i - 2] > k) {
+                return new int[][]{};
+            }
+            ans[i / 3] = new int[]{nums[i - 2], nums[i - 1], nums[i]};
+        }
+        return ans;
+    }
+
+    /**
      * 1922. 统计好数字的数目
      * @param n
      * @return
